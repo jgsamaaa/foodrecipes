7f11a6027541eb63f7ee2cfb251315640a80d648
James Gabriel Dumaug <jamesgabrieldumaug@gmail.com>
2022-09-28 20:56:35 -0600

    Food recipe website starting code.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center p-20 bg-blue-400 font-bold text-5xl">
+        Startin code
+      </h1>
+    </div>
+  );
+}
+
+export default App;
