25094489c5c1dc22aea7a7e5f0679418aeb1ab2b
James Gabriel Dumaug <jamesgabrieldumaug@gmail.com>
2022-09-28 21:59:16 -0600

    components and pages folder

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Serach = () => {
+  return (
+    <div>
+      <h1>Serach</h1>
+    </div>
+  );
+};
+
+export default Serach;
---
+++ src/pages/Searched.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Searched = () => {
+  return (
+    <div>
+      <h1>Searched</h1>
+    </div>
+  );
+};
+
+export default Searched;
